cc3a51f25112f90877e1d87a6e39be8dfbca3927
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-08-11 16:10:40 +0900

    modified:   pages/api/webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const crypto = require('crypto');
 
-export default function webhook(req, res) {
+export default async function webhook(req, res) {
   const query = req.query;
   const { consumer_key , crc_token } = query;
 
   return(
     res.status(200).json({
-      response_token: crypto.createHmac('sha256', consumer_key).update(query.crc_token).digest('base64')
+      response_token: crypto.createHmac('sha256', consumer_key).update(crc_token).digest('base64')
     })
-  )
+  );
 }
