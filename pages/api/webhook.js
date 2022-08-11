a8cdc772b1172d4f3a189dc155cf4dcd3880f09a
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-08-11 19:28:41 +0900

    modified:   .gitignore 	modified:   pages/api/webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -5,7 +5,7 @@ export default async function webhook(req, res) {
   const query = req.query;
   const { crc_token } = query;
 
-  if(crc_token == undefined){
+  if(crc_token == undefined || crc_token == ''){
     return(
       res.status(201).json({
         errors: [
@@ -20,7 +20,7 @@ export default async function webhook(req, res) {
   
   return(
     res.status(200).json({
-      response_token: 'sha256=' + crypto.createHmac('sha256', process.env.CONSUMER_SECRET).update(crc_token).digest('base64')
+      response_token: `sha256=${crypto.createHmac('sha256', process.env.CONSUMER_SECRET).update(crc_token).digest('base64')}`
     })
   );
 }
