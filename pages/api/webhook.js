38baa56cf1dd4514f5bd72384c09409be44744c4
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-08-11 16:29:40 +0900

    modified:   pages/api/webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -3,11 +3,11 @@ const crypto = require('crypto');
 
 export default async function webhook(req, res) {
   const query = req.query;
-  const { consumer_key , crc_token } = query;
+  const { crc_token } = query;
 
   return(
     res.status(200).json({
-      response_token: crypto.createHmac('sha256', consumer_key).update(crc_token).digest('base64')
+      response_token: crypto.createHmac('sha256', process.env.consumer_secret).update(crc_token).digest('base64')
     })
   );
 }
