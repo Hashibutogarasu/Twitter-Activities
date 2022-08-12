510ec841b0d2bd38aa632c3fe0b748c798c09f88
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-08-12 15:01:00 +0900

    modified:   pages/api/webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,12 +1,6 @@
-
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const crypto = require('crypto');
 const { NextApiRequest, NextApiResponse } = require('next/dist/shared/lib/utils');
-const ws = new WebSocket(`$ws://${process.env.NEXT_PUBLIC_IP_PORT_WS}`);
-
-ws.onopen = e => {
-    console.log('接続ヨシ！')
-}
 
 /**
  * 
@@ -53,6 +47,12 @@ export default async function webhook(req, res) {
                 message: `Cant send.`
             });
         });
+
+        const ws = new WebSocket(`$ws://${process.env.NEXT_PUBLIC_IP_PORT_WS}`);
+
+        ws.onopen = e => {
+            console.log('接続ヨシ！');
+        }
 
         ws.send(request_json);
         
